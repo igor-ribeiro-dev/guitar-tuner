99cc7839c8aabc4c5c0955cb4653df308d755a74
Igor Ribeiro <igorribeiro4467@gmail.com>
2023-10-12 13:37:10 -0300

    Initial Commit

---
+++ GuitarTuner Watch App/GuitarTunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuitarTunerApp.swift
+//  GuitarTuner Watch App
+//
+//  Created by Igor Ribeiro on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GuitarTuner_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
